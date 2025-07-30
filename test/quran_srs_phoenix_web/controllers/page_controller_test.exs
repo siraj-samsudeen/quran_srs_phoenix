6d13d0499a9947073df97709a0330b5fa6925fbf
Siraj Samsudeen <mailsiraj@gmail.com>
2025-07-30 09:40:30 +0530

    set up home page with links for testing easily

--- test/quran_srs_phoenix_web/controllers/page_controller_test.exs
+++ test/quran_srs_phoenix_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule QuranSrsPhoenixWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Quran SRS Phoenix - Testing Interface"
   end
 end
